7b54dc7df35eaf69f7cf1286ba89bbe8ca602895
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-06-25 21:02:23 -0300

    :sparkles: feat: adding num check and displaying result message on homepage

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,43 @@
-let pin = Math.floor(Math.random() * 1000)
+let pin = Math.floor(Math.random() * 9000) + 1000
 console.log(pin)
-document.getElementById('pin').value = pin
-let verifyButton = document.getElementById('verifyButton')
+const verifyButton = document.getElementById('verifyButton')
 
+//Calling the verify function when button pressed
 verifyButton.addEventListener('click', function () {
-  verifyPin()
+  validatePin()
 })
 
-function verifyPin() {
+function createResultMessage(message, success) {
+  let resultMessage = document.getElementById('result')
+  resultMessage.textContent = message
+  resultMessage.style.color = success ? 'green' : 'red'
+}
+
+function validatePin() {
   let guess = document.getElementById('guess').value
+  if (guess == '' || isNaN(guess) || guess < 0) {
+    createResultMessage('Por favor entre um número válido', false)
+    return
+  }
+  guess = Number(guess)
 
   if (guess > pin + 100) {
-    alert('Errou! Informe um valor bem menor')
+    createResultMessage('Errou! Informe um valor bem menor', false)
     return
   }
   if (guess < pin - 100) {
-    alert('Errou! Informe um valor bem maior')
+    createResultMessage('Errou! Informe um valor bem maior', false)
     return
   }
 
   if (guess > pin) {
-    alert('Errou! Passou por pouco!')
+    createResultMessage('Errou! Passou por pouco!', false)
     return
   }
   if (guess < pin) {
-    alert('Errou! Faltou pouco!')
+    createResultMessage('Errou! Faltou pouco!', false)
     return
   }
 
-  alert('Acertou!!')
+  createResultMessage('Acertou!!', true)
 }
