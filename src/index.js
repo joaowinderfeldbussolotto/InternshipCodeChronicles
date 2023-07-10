da66f0036aea06a8c50cac73ed9162fe1985e8c2
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-07-10 15:42:10 -0300

    :sparkles: feat: adding request to GIPHY API

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const config = require('./config');
+
+
+app.get('/api/gifs', (req, res) => {
+    const giphyApiKey = config.giphyApiKey;
+    const search = req.query.search || 'cats';
+
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&limit=3&q=${search}`;
+    axios
+        .get(url)
+        .then(response => {
+            const gifData = response.data;
+            if (gifData.data.length > 0) {
+                const keys = Object.keys(gifData);
+                console.log(keys); // Print the keys to the console
+                res.json(gifData.data);
+            } else {
+                res.status(404).json({ message: 'No GIF found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
