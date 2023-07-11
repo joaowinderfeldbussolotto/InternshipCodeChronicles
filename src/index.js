399dcfdf8b1bb6f75b738a031c980e53048e22a3
luizpgt <luiz.paulus51@gmail.com>
2023-07-11 11:53:57 -0300

    feat: front n back using hbs + simple home page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 const config = require('./config');
+
+// * ATENCAO *
+// TODO: precisamos organizar a questao de uso de ; em finais de linha
+// TODO: 
 
 app.get('/api/comic', (req,res) => {
     let comicNumber = ''
@@ -35,6 +41,23 @@ app.get('/api/gifs', (req, res) => {
             return res.status(500).json({ error: 'Internal Server Error' });
         });
 });
+
+function getLastWComic() {
+  return axios.get(`https://xkcd.com/info.0.json`)
+    .then(comic=> comic.data);
+}
+
+app.get('/', (req, res) => {
+  getLastWComic().then(wcData => {
+    res.render('index',  // index esta no /layout/index.handlebars
+      {wcomic: wcData});
+  });
+});
+
+// handlebars
+app.set('views', path.join(__dirname, 'views')); // onde estao as views
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'})); // arquivo principal de layout
+app.set('view engine', 'handlebars'); // qual a view engine
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
