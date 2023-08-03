29ca63f94bc96bb42d17a53f09d740cd8c18d5bd
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-08-03 15:22:20 -0300

    :package: env: setting 8080 as the default PORT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const CHUCKNORRIS_API_URL = process.env.CHUCKNORRIS_API_URL;
 const BORED_API_URL  = process.env.BORED_API_URL;
 
