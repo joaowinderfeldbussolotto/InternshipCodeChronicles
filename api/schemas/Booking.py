e5c904a2403a1fec0add16416a01e787d10acd08
luizpgt <luiz.paulus51@gmail.com>
2023-08-21 02:59:48 -0300

    :sparkles: feat: api route getting predictions from sagemaker xgboost endpoint

--- api/schemas/Booking.py
+++ api/schemas/Booking.py
@@ -3,16 +3,17 @@ from pydantic import BaseModel
 class Booking(BaseModel):
     no_of_adults: int
     no_of_children: int
-    type_of_meal_plan: str
     no_of_weekend_nights: int
     no_of_week_nights: int
     required_car_parking_space: int
-    room_type_reserved: str
     lead_time: int
     arrival_year: int
     arrival_month: int
     arrival_date: int
-    market_segment_type: str
     repeated_guest: int
     no_of_special_requests: int
-    booking_status: str
+    no_of_previous_cancellations: int
+    no_of_previous_bookings_not_canceled: int
+    type_of_meal_plan: str
+    room_type_reserved: str
+    market_segment_type: str
