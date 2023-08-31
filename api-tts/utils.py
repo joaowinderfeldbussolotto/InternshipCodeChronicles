d50b507cf8ae37574db7b58d6bbe0c2c46ada366
Luiz Augusto Scarsi <116020373+LuizScarsi@users.noreply.github.com>
2023-08-31 14:56:49 -0300

    :sparkles: feat: add hash creation function

--- api-tts/utils.py
+++ api-tts/utils.py
@@ -1,4 +1,10 @@
-from datetime import datetime 
+from datetime import datetime
+import hashlib
 
 def getFormattedDateTime():
     return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+def create_hash(text):
+    text_hash = hashlib.sha256()
+    text_hash.update(text.encode('utf-8'))
+    return text_hash.hexdigest()
