717af018e6eb1f6ba8bdff4f0f455a6db075d5ee
Josué Fernandes <josue7mf@hotmail.com>
2023-09-04 01:29:51 -0300

    :sparkles: feat: Add audio view in frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,15 @@
-// js code
+
+const main = document.querySelector(".main-content")
 const inputContent = document.querySelector(".main-content.input")
 const outputContent = document.querySelector(".main-content.output")
 const form = document.querySelector("#form")
 const submit = document.querySelector("#submit")
 const phraseInput = document.querySelector("#phrase")
 const tryAgain = document.querySelector(".try-again")
-const BASE_URL = "https://wkiybux0uj.execute-api.us-east-1.amazonaws.com/"
+const BASE_URL = "https://sjcz8s68n4.execute-api.us-east-1.amazonaws.com/"
 const jsonContainer = document.querySelector("#jsonContainer")
+const audioInput = document.querySelector(".audio")
+
 let url = ""
 
 
@@ -17,7 +20,8 @@ async function handleForm(e) {
   e.preventDefault()
   if (phraseInput.value !== "" && url !== "") {
     const response = await fetchData(url, phraseInput.value)
-    handleState()
+    const audio = createAudio(JSON.parse(response)['url_to_audio'])
+    handleState(audio)
     jsonContainer.textContent = response
   } else if (phraseInput.value == "") {
     alert("Por favor digite sua frase:")
@@ -47,12 +51,14 @@ async function fetchData(url, phrase) {
   }
 }
 
-function handleState() {
+function handleState(audio) {
 
   if (getComputedStyle(outputContent).display == 'none') {
     outputContent.style.display = "block"
     inputContent.style.display = 'none'
     tryAgain.style.visibility = 'visible'
+    audioInput.style.visibility = 'visible'
+    audioInput.append(audio)
   } else {
     outputContent.value = ""
     outputContent.style.display = "none"
@@ -60,5 +66,23 @@ function handleState() {
     url = ""
     phraseInput.value = ""
     tryAgain.style.visibility = 'hidden'
+    audioInput.style.visibility = 'hidden'
+    if(audioInput.children[0]) audioInput.children[0].remove()
+    
   }
+}
+
+function createAudio(url_to_audio) {
+  const audio = document.createElement('audio')
+  audio.setAttribute('controls', 'controls')
+
+  const sourceElement = document.createElement('source');
+  sourceElement.setAttribute('src', url_to_audio);
+  sourceElement.setAttribute('type', 'audio/mpeg');
+
+  const unsupportedMessage = document.createElement('p');
+  unsupportedMessage.textContent = 'Seu navegador não suporta a reprodução de áudio.';
+
+  audio.appendChild(...[sourceElement,unsupportedMessage]);
+  return audio
 }
