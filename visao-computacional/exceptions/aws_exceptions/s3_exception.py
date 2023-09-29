3737e349ea3493ac133587bdd60279ba885031a0
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-09-29 00:13:13 -0300

    :bug: fix: bug on NoSuchKey exception fixed

--- visao-computacional/exceptions/aws_exceptions/s3_exception.py
+++ visao-computacional/exceptions/aws_exceptions/s3_exception.py
@@ -8,17 +8,22 @@ class S3Exception(BaseException):
     def handle_s3_exception(error_code):
 
         s3_exceptions = {
-            "404": S3BucketNotFoundException(),
-            "403": S3BucketAccessDeniedException()
+            "NoSuchBucket": S3BucketNotFoundException(),
+            "NoSuchKey": S3KeyNotFoundException(),
+            "AccessDenied": S3BucketAccessDeniedException()
         }
 
         return s3_exceptions.get(error_code, S3Exception())
 
 class S3BucketNotFoundException(BaseException):
-    def __init__(self,message="The requested resource could not be found. Please double-check the bucket and image name and try again.",
+    def __init__(self,message="The requested resource could not be found. Please double-check the bucket name and try again.",
                  status_code=404):
         super().__init__(message, status_code)
 
 class S3BucketAccessDeniedException(BaseException):
     def __init__(self, message="You don't have permission to access this bucket", status_code=403):
+        super().__init__(message, status_code)
+
+class S3KeyNotFoundException(BaseException):
+    def __init__(self, message="The requested file could not be found in the bucket. Please double-check the imageName and try again", status_code=404):
         super().__init__(message, status_code)
--- visao-computacional/services/s3_service.py
+++ visao-computacional/services/s3_service.py
@@ -1,5 +1,6 @@
 import pytz
 import boto3
+from botocore.exceptions import ClientError
 from core.config import settings
 from exceptions.aws_exceptions.s3_exception import S3Exception
 def created_image_datetime(imageName, bucketName=settings.BUCKET_NAME):
@@ -13,10 +14,11 @@ def created_image_datetime(imageName, bucketName=settings.BUCKET_NAME):
      bucketName = settings.BUCKET_NAME
     try:
         s3 = boto3.client('s3')
-        imageInfo = s3.head_object(Bucket=bucketName, Key=imageName)
+        imageInfo = s3.get_object_attributes(Bucket=bucketName, Key=imageName, ObjectAttributes = ['ETag'])
         tz = pytz.timezone('America/Sao_Paulo')
         response = imageInfo['LastModified'].astimezone(tz).strftime("%d-%m-%Y %H:%M:%S")
         return response
-    except Exception as e:
+    except ClientError as e:
         error_code = e.response['Error']['Code']
         raise S3Exception.handle_s3_exception(error_code)
+   
