8179be04f93091335a6de657731ab896acfc36ed
Josué Fernandes <josue7mf@hotmail.com>
2023-09-29 15:02:01 -0300

    :sparkles: feat: Add exceptions messages from env file

--- visao-computacional/exceptions/aws_exceptions/rekognition_exception.py
+++ visao-computacional/exceptions/aws_exceptions/rekognition_exception.py
@@ -1,7 +1,7 @@
 from exceptions.base_exception import BaseException
-
+from core.exceptions import exceptions
 class RekognitionException(BaseException):
-    def __init__(self,message="An error ocurred in Amazon Rekognition", status_code=500):
+    def __init__(self,message=exceptions.REKOGNITION_DEFAULT_ERROR, status_code=500):
         super().__init__(message, status_code)
 
     @staticmethod
@@ -21,10 +21,10 @@ class RekognitionException(BaseException):
         return rekognition_exceptions.get(error_code, RekognitionException())
 
 class RekognitionInvalidS3ObjectException(RekognitionException):
-    def __init__(self, message="Invalid S3 Object or S3 Image"):
+    def __init__(self, message=exceptions.REKOGNITION_INVALID_S3_ERROR):
         super().__init__(message,status_code=404)
 
 class RekognitionRateLimitExceededException(RekognitionException):
-    def __init__(self, message="Rate limit exceeded while processing the request"):
+    def __init__(self, message=exceptions.REKOGNITION_RATE_LIMIT_ERROR):
         super().__init__(message, status_code=429)
 
--- visao-computacional/exceptions/aws_exceptions/s3_exception.py
+++ visao-computacional/exceptions/aws_exceptions/s3_exception.py
@@ -1,7 +1,9 @@
 from exceptions.base_exception import BaseException
+from core.exceptions import exceptions
+
 
 class S3Exception(BaseException):
-    def __init__(self,message="An error ocurred in Amazon S3", status_code=500):
+    def __init__(self, message=exceptions.S3_DEFAULT_ERROR, status_code=500):
         super().__init__(message, status_code)
 
     @staticmethod
@@ -15,15 +17,18 @@ class S3Exception(BaseException):
 
         return s3_exceptions.get(error_code, S3Exception())
 
+
 class S3BucketNotFoundException(BaseException):
-    def __init__(self,message="The requested resource could not be found. Please double-check the bucket name and try again.",
+    def __init__(self, message=exceptions.S3_BUCKET_NOT_FOUND_ERROR,
                  status_code=404):
         super().__init__(message, status_code)
 
+
 class S3BucketAccessDeniedException(BaseException):
-    def __init__(self, message="You don't have permission to access this bucket", status_code=403):
+    def __init__(self, message=exceptions.S3_ACCESS_DENIED_ERROR, status_code=403):
         super().__init__(message, status_code)
 
+
 class S3KeyNotFoundException(BaseException):
-    def __init__(self, message="The requested file could not be found in the bucket. Please double-check the imageName and try again", status_code=404):
+    def __init__(self, message=exceptions.S3_KEY_NOT_FOUND_ERROR, status_code=404):
         super().__init__(message, status_code)
